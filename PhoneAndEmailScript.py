9ec3ceeec3a622212f9b8cf65f55cf324b7853bf
Adnan Malik <adnanmalikinfo110@gmail.com>
2021-01-04 19:27:03 -0800

    Testing commit

--- PhoneAndEmailScript.py
+++ PhoneAndEmailScript.py
@@ -2,9 +2,39 @@
 
 #UNDER DEVELOPMENT
 
-#A script to etrieve all of phon numbers and email from a site or file
+#A script to retrieve all of phon numbers and email from a site or file
 
-import re, pyperclip
+import re
+import pyperclip
 
 #Create a regex of phone Number
+
+phoneRegex = re.compile(r'''
+ (
+ 0|\+92              #Country Code
+ \d\d\d              #Network Code
+ \d\d\d\d\d\d\d 
+ )   #Contact Number
+ ''', re.VERBOSE)
+
+#Create Regex for Email addresses
+
+emailRegex = re.compile(r'''
+ [a-zA-Z0-9_.+] +   #namepart
+ @                  #@ symbol
+ [a-zA-Z0-9_.+] +    #domain name part
+
+ ''',  re.VERBOSE)
+ 
+result = pyperclip.paste()
+extractedPhoneNumbers = phoneRegex.findall(result)
+extractedEmails = emailRegex.findall(result)
+
+print(extractedPhoneNumbers)
+print(extractedEmails)
+
+
+
+
+
 
